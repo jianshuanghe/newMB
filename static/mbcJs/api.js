37dca97c620ad40d3032beb121c528758c6d501b
jianshuanghe <274088960@qq.com>
2020-03-11 10:00:16 +0800

    优化

--- static/mbcJs/api.js
+++ static/mbcJs/api.js
@@ -13,7 +13,7 @@ if (process.env.NODE_ENV === 'development') {
 	// api3 = 'https://img01.iambuyer.com.cn';
 	//正式
 	api1 = 'https://api.iambuyer.com.cn';
-	api2 = 'https://api.ruhexiu.com';
+	api2 = 'https://zhaojie0001.mynatapp.cc';
 	api3 = 'https://img01.iambuyer.com.cn'; // 图片
 }
 
